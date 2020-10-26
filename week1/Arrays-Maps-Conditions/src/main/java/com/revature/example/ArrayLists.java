f76e65197ce4f7df54c09b1a0ed4657c29079dec
sophiagavrila <msophiagavrila@gmail.com>
2020-10-26 14:56:16 -0400

    push arraylists

--- week1/Arrays-Maps-Conditions/src/main/java/com/revature/example/ArrayLists.java
+++ week1/Arrays-Maps-Conditions/src/main/java/com/revature/example/ArrayLists.java
@@ -1,10 +1,39 @@
 package com.revature.example;
 
+import java.util.ArrayList;
+
 public class ArrayLists {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * In addition to arrays Java provides ArrayLists
+		 * This is a data type that automatically grows and shrinks
+		 * an underlting array, so you're not locked into using an array
+		 * of a predetermined size.
+		 */
+		
+		// ArrayLists are objects, so you instantiate them using constructors
+		ArrayList<String> triumvers = new ArrayList<String>();
+		
+		triumvers.add("Julius Caeser");
+		triumvers.add("Cassius");
+		triumvers.add("Pompeius");
+		
+		// to retrieve objects from an ArrayList, use the .get() method.
+		System.out.println(triumvers.get(1));
+	
+		System.out.println(triumvers);
+		
+		// System.out.println(triumvers.remove(0));
+		
+		triumvers.add("Brutus");
+		
+		System.out.println(triumvers);
+		
+		triumvers.set(0, "Juliu$ Seiza");
+		
+		System.out.println(triumvers);
+		
 	}
 
 }
---
+++ week1/Arrays-Maps-Conditions/src/main/java/com/revature/example/HashMaps.java
@@ -0,0 +1,5 @@
+package com.revature.example;
+
+public class HashMaps {
+
+}
