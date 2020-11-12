ff00a2c9f16e2a17b438c973ba2cd7fe50138675
sophiagavrila <msophiagavrila@gmail.com>
2020-11-12 13:04:20 -0500

    complete ConnectionUtil

---
+++ week3/EmployeeServletDemo/src/main/java/com/revature/models/Employee.java
@@ -0,0 +1,151 @@
+package com.revature.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+// this is a BEAN!
+
+// 1. All properties are private
+// 2. A public no-args constructor
+// 3. It implements Serializable
+public class Employee implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+// Serializable objects can be written to streams, files, and object databases
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	
+	private LocalDateTime hireDate;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(int id, String firstName, String lastName, String username, String password,
+			LocalDateTime hireDate) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.hireDate = hireDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDateTime getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(LocalDateTime hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((hireDate == null) ? 0 : hireDate.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	// This method assures that if two objects have the same values, they are equal
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (hireDate == null) {
+			if (other.hireDate != null)
+				return false;
+		} else if (!hireDate.equals(other.hireDate))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
+				+ ", password=" + password + ", hireDate=" + hireDate + "]";
+	}
+	
+	
+}
---
+++ week3/EmployeeServletDemo/src/main/java/com/revature/repositories/EmployeeDAOImpl.java
@@ -0,0 +1,30 @@
+package com.revature.repositories;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.revature.models.Employee;
+
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+	private static Logger log = Logger.getLogger(EmployeeDAOImpl.class);
+	
+	public boolean insert(Employee e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean update(Employee e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
