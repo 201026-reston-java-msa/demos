8a5262974940ec16dae7521e498842639a47e925
sophiagavrila <msophiagavrila@gmail.com>
2020-11-18 13:10:36 -0500

    HelloHibernate 90% done

---
+++ week4/HelloHibernate/src/main/java/com/revature/driver/Driver.java
@@ -0,0 +1,33 @@
+package com.revature.driver;
+
+public class Driver {
+
+	
+	public static void main(String[] args) {
+		initialValues();
+		// run a selectAll statment from one of our Daos...like SuperPrisonDao
+		
+		// close the Hibernate session here...
+		
+	}
+	
+	public static void initialValues() {
+		// Create new Crimes
+		
+		// Creaete a a new CrimeDao
+		
+		// use the CrimeDao to insert the crimes
+		
+		
+		// Create Super Prisons,
+		
+		// Create a SuperPrisonDao
+		
+		// Create SuperVillains
+		
+		
+	}
+	
+	
+	
+}
---
+++ week4/HelloHibernate/src/main/java/com/revature/model/Crime.java
@@ -0,0 +1,67 @@
+package com.revature.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Crime {
+	
+	@Id
+	@Column(name="crime_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int crimeId;
+	
+	@Column(name="crime_name", unique=true, nullable=false)
+	private String crimeName;
+	
+	@Column(name="description")
+	private String description;
+	
+	public Crime() {
+		
+	}
+
+	public Crime(int crimeId, String crimeName, String description) {
+		super();
+		this.crimeId = crimeId;
+		this.crimeName = crimeName;
+		this.description = description;
+	}
+
+	public Crime(String crimeName, String description) {
+		super();
+		this.crimeName = crimeName;
+		this.description = description;
+	}
+
+	public int getCrimeId() {
+		return crimeId;
+	}
+
+	public void setCrimeId(int crimeId) {
+		this.crimeId = crimeId;
+	}
+
+	public String getCrimeName() {
+		return crimeName;
+	}
+
+	public void setCrimeName(String crimeName) {
+		this.crimeName = crimeName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Crime [crimeId=" + crimeId + ", crimeName=" + crimeName + ", description=" + description + "]";
+	}
+	
+}
